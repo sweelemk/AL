e487095d9b5e4d5919b636b7c51c8cf07450e31a
Andrey Kashkan <andrey.kashkan@globant.com>
2022-06-28 17:38:52 +0300

    Basic app setup

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Hello AtixLabs</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
